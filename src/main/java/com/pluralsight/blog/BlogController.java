2f25291b99084e8413e513e46538b6f907871d5d
ssv-cg <siddhant.vispute@capgemini.com>
2023-01-31 18:20:43 +0530

    Display a Post Details Page

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.pluralsight.blog.data.PostRepository;
@@ -25,5 +26,12 @@ public class BlogController {
 		model.put("title", "Blog Post 1");
 		model.put("posts", listOfPosts);
 		return "home";
+	}
+	
+	@RequestMapping("/post/{id}")
+	public String postDetails(@PathVariable Long id,ModelMap model) {
+		Post post= postRepository.findById(id);
+		model.put("post", post);
+		return "post-details";
 	}
 }
